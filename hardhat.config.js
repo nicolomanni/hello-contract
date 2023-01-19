41ccee4126d4c0510576520df6be19756121f251
Nicolò Manni <nicolo.manni@evologi.it>
2023-01-19 16:40:00 +0100

    Real blockchain configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,12 +11,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
 };
